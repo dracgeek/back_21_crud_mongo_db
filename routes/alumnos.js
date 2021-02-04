a743407028ac8af9c603db162fc64b45fab46a25
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2021-02-04 10:59:08 -0500

    first commit

---
+++ routes/alumnos.js
@@ -0,0 +1,69 @@
+const { Router} = require('express');
+const router = Router();
+const connection = require('../db')
+
+
+const {ObjectId} = require('mongodb');
+
+
+router.get('/', async(req,res)=>{
+     
+    const db = await connection();
+    await db.collection('alumnos').find()
+    .toArray(function(err,alumnos){
+        res.json(alumnos)
+    })
+
+});
+
+router.post('/', async (req,res)=>{
+    const db = await connection();
+    const { nombres, apellidos, edad} = req.body;
+
+    db.collection('alumnos').insertOne({
+        nombres,
+        apellidos,
+        edad
+    }, function(
+        err,
+        info
+    ){
+        res.json(info.ops[0]);
+    })
+
+});
+
+
+router.put('/:id', async (req,res)=>{
+    const db = await connection();
+    const { nombres, apellidos, edad} = req.body;
+    const id = req.params.id;
+
+    db.collection('alumnos').findOneAndUpdate(
+        {"_id":ObjectId(id)},
+        {$set:{nombres:nombres,apellidos:apellidos,edad:edad}},
+       // {returnNewDocument: true},
+        function(){
+           res.json('Alumno actualizado');
+        }
+    )
+});
+
+router.delete('/:id', async (req,res)=>{
+
+    const db = await connection();
+    const id = req.params.id;
+
+    db.collection('alumnos').deleteOne(
+        {"_id":ObjectId(id)},
+        function(){
+            res.json({message:'Alumno eliminado'});
+        }
+    )
+})
+
+
+
+
+
+module.exports = router;
